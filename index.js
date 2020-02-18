980a9681f8c7a684622fd039aed4d9237c247156
“ievastumpe” <ieva.stumpe@gmail.com>
2020-02-18 11:01:55 -0300

    updated files with little details - all works

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-module.exports = () => {
-  // ...
+// Importar modulo File System (fs)
+const fs = require('fs');
+
+module.exports = (filePath, options) => {
+  return new Promise((resolve, reject)=> {
+    // Leer el archivo
+    fs.readFile(filePath, function(err, data){
+      if (err){
+        return reject(err);
+      }
+      resolve(data.toString());
+    });
+  });
 };
--- subfunctions/02-links.js
+++ subfunctions/02-links.js
@@ -1,13 +1,9 @@
-// const mdLinks = require('./index');
-// const fetch = require('node-fetch');
-// const chalk = require('chalk');
-// const {analyseLinks, extactData} = require('./02-links');
-
 const fs = require('fs');
 const path = require ('path');
 const file = process.argv[2];
 const pathAbsolute = path.resolve(file);
 const markdownLink = require('markdown-link-extractor');
+
 
 exports.readFile = (pathAbsolute) => {
     return new Promise((resolve, reject) => {
@@ -21,33 +17,3 @@ exports.readFile = (pathAbsolute) => {
 exports.readFile(pathAbsolute)
     .then(data => console.log(markdownLink(data)))
     .catch(error => console.log(error));
-
-
-
-
- 
-// const getFile = file => {
-//     return new Promise((resolve, reject) => {
-//       fs.readFile(file, 'utf8', (err, data) => {
-//         if (err) {
-//           return reject(err);
-//         } else {
-//           let links = [];
-//           const renderer = new marked.Renderer(); // Get reference
-//           renderer.link = function(href, title, text) {
-//             // Override function
-//             links.push({
-//               href: href,
-//               text: text,
-//               filePath: file
-//             });
-//           };
-//           marked(data, { renderer: renderer }); // Aquí imprime y crea los elementos dentro del Array
-//           links = linksHttp(links);
-//           resolve(links);
-//         }
-//       });
-//     });
-//   };
-
-// module.exports = readFile; 
