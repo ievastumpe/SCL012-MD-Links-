34cac8c0bcd2858c7871b12e9faabf289acbd911
“ievastumpe” <ieva.stumpe@gmail.com>
2020-02-18 11:03:25 -0300

    changes in statistics file

--- subfunctions/03-statistics.js
+++ subfunctions/03-statistics.js
@@ -1,21 +1,12 @@
 'use strict'
-//const mdLinks = require('./index');
 const fs = require('fs');
-const path = require('path'); 
-const folder = process.argv[2];
-const pathAbsolute = path.resolve(folder);
-const link = ('http'+'');
-
-const analyseLinks = (data) => {
-  const regExp = /https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,}/g;
-  const matchLinks = data.match(regExp);
-  return matchLinks;
-};
+const path = require ('path');
+const file = process.argv[2];
+const pathAbsolute = path.resolve(file);
+const markdownLink = require('markdown-link-extractor');
 
 //------counting links
 fs.readFile(pathAbsolute, 'utf8', function (err, content) {
-  const numberLinks = content.split(regExp).length - 1;
+  const numberLinks = content.split(markdownLink).length - 1;
   console.log(numberLinks);
 });
-
-module.exports = {analyseLinks};
